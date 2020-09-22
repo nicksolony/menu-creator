bcf1fd4a1ed09c808ce32ddb964d2d09c4558ab2
Nick <github email address>
2020-09-22 00:04:48 -0400

    populated seeds for categories

--- menu-creator-backend/db/seeds.rb
+++ menu-creator-backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#   Seeding Categories
+Category.create(name:'Desserts');
+Category.create(name:'Appetizers');
+Category.create(name:'Entrees');
+Category.create(name:'Drinks');
