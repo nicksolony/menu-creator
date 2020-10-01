5a09f6ee84f3e241d0a368dfa1cf120d732374c2
Nick <github email address>
2020-09-30 21:46:34 -0400

    created index and show contrikker actions

--- menu-creator-backend/app/models/menu.rb
+++ menu-creator-backend/app/models/menu.rb
@@ -1,5 +1,7 @@
 class Menu < ApplicationRecord
   has_many :menu_items
+  has_many :items, :through => :menu_items
+  has_many :categories, :through => :items
   validates :name, presence: true
   validates :name, uniqueness: true
 
