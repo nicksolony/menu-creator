9e43f62fea9794cb5415c8857944c0fc10ea21aa
Nick <github email address>
2020-10-03 15:28:03 -0400

    menu_item create controller

--- menu-creator-backend/app/controllers/menu_items_controller.rb
+++ menu-creator-backend/app/controllers/menu_items_controller.rb
@@ -1,2 +1,22 @@
 class MenuItemsController < ApplicationController
+  def create
+    @menu_item = MenuItem.new(menu_item_params)
+
+    if @menu_item.save
+      render json: @menu_item, status: :created, location: @menu_item
+    else
+      render json: @menu_item.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    # def set_menu
+    #   @menu_item = Menu.find(params[:id])
+    # end
+
+    # Only allow a trusted parameter "white list" through.
+    def menu_item_params
+      params.require(:menu_item).permit(:menu_id,:item_id)
+    end
 end
